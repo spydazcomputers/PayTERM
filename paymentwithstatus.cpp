5a54a9b8e38a3de9cedf6d8045937743c8a77069
Ryan <spydazcomputers@gmail.com>
2015-03-31 11:42:21 -0400

    Removed Legacy non working code and added some functional comments

--- paymentwithstatus.cpp
+++ paymentwithstatus.cpp
@@ -13,46 +13,7 @@ PaymentWithStatus::PaymentWithStatus(QWidget *parent) :
 
 void PaymentWithStatus::SetReply(const QVariantMap &reply)
 {
-    qDebug ()<< "made it this far";
-
-    /* QStringList SplitURL;
-     SplitURL = reply["status_url"].toString().split('?');
-     qDebug () << SplitURL;
-     QByteArray data;
-     data.append(SplitURL.last());
-     QByteArray PubKey = "a48ef0ca947fc892b79e07129bae15a80ba54d54d6ea42fa4c50cb14a34c96d5";
-     QByteArray PrivKey = "1C0AEf6D6289D0bB0aaffa47ad3Aa8B3Ea0F4589419834Bdc845a3fF8774223a";
-
-      // list: ["Arial", "Helvetica", "Times", "Courier"]
-     QByteArray HMAC = QMessageAuthenticationCode::hash(data, PrivKey, QCryptographicHash::Sha512).toHex();
-
-     QUrl qrl("https://www.coinpayments.net/index.php");
-     qDebug() << "Calling url: " << qrl.toString();
-     QNetworkAccessManager *manager = new QNetworkAccessManager(this);
-     QNetworkRequest request(qrl);
-
-     request.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
-     request.setRawHeader("HMAC", HMAC);
-    // qDebug() << request.rawHeader("HMAC:");
-
-     QNetworkReply *netreply = manager->post(request, data);
-     QEventLoop eventLoop;
-     connect(netreply, SIGNAL(finished()), &eventLoop, SLOT(quit()));
-     eventLoop.exec();
-     if (netreply->error() == QNetworkReply::NoError)
-     {
-         QString strReply = (QString)netreply->readAll();
-
-         //parse json
-         qDebug() << "Response:" << strReply;
-         QJsonDocument jsonResponse = QJsonDocument::fromJson(strReply.toUtf8());
-         QJsonObject jsonObj2;
-         QJsonObject jsonObj = jsonResponse.object();
-         // Access subtier of JSON result and store in DB
-
-        jsonObj2 = jsonObj["result"].toObject();
-     }
-*/
+    //Load the status_url of the order created on the Create_order page (From Coin Payments)
      ui->webView->setUrl(reply["status_url"].toString());
 
 }
@@ -66,6 +27,7 @@ PaymentWithStatus::~PaymentWithStatus()
 
 void PaymentWithStatus::on_webView_loadFinished(bool arg1)
 {
+    //Scroll the webview box to the middle to display the payment info but not the header.
     ui->webView->page()->mainFrame()->setScrollBarValue(Qt::Vertical, ((ui->webView->page()->mainFrame()->scrollBarMaximum(Qt::Vertical)/2)));
 
 }
